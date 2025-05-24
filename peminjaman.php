3ae4817df434e3139aaafe9dde4330d658b5b5e0
Yusuf Abdillah <yusufabdillah2306@gmail.com>
2025-05-25 01:16:07 +0700

    tes deploy

--- peminjaman.php
+++ peminjaman.php
@@ -1,4 +1,4 @@
-<html lang="en">
+<!-- <html lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -36,4 +36,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 
-</html>
+</html> -->
