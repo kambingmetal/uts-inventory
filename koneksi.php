d110c0c6df9eb1b6c5af6577fd3538d6d0a2be91
Yusuf Abdillah <yusufabdillah2306@gmail.com>
2025-05-24 22:39:11 +0700

    (feat) add env

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_inventory";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['MYSQLHOST'];
+$user = $_ENV['MYSQLUSER'];
+$pass = $_ENV['MYSQLPASSWORD'];
+$db   = $_ENV['MYSQLDATABASE'];
+$port = $_ENV['MYSQLPORT'];
 
 $conn = new mysqli($host, $user, $pass, $db);
 
